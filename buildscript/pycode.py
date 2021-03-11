eb02e9a729466f0ef08d2568c9e8d70b49ed9657
sinaden <42101013+sinaden@users.noreply.github.com>
2021-03-11 18:57:29 +0100

    Update pycode.py

--- buildscript/pycode.py
+++ buildscript/pycode.py
@@ -1,5 +1,16 @@
-print("hello")
+from datetime import datetime
 
-f = open("newfile.txt", "w")
-f.write("lorenipsum")
+# datetime object containing current date and time
+now = datetime.now()
+
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)	
+
+
+print("hello, " + dt_string)
+
+f = open("newfile.txt", "a")
+f.write("\n The file was updated at "+ dt_string)
 f.close()
